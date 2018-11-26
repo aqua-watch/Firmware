df66b24d2529e1271ea65e12269c25b8267cb8c3
Armin Sabouri <armins@mit.edu>
2018-11-26 13:01:41 -0500

    display current samples in table

--- arm/web_interface/app.py
+++ arm/web_interface/app.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask import render_template
+from flask import Markup
+
+
+import sys
+sys.path.insert(0, '../../')
+from formatModels import get_absolute_values
+from formatModels import get_normalized_values
+#sys.path.append('arm/web_interface/')
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def display_content():
-    return ''
+    
+    norm_df = get_normalized_values()
+    absolute_df = get_absolute_values()
+    
+    
+    norm_html = Markup(norm_df.to_html())
+    absolute_html = Markup(absolute_df.to_html())
+    
+    
+    
+    return render_template('content.html', normalized=norm_html, absolute=absolute_html) 
--- formatModels.py
+++ formatModels.py
@@ -6,12 +6,12 @@ Created on Wed Oct 24 16:16:24 2018
 """
 import json
 import pandas as pd
-
+import os, sys
 
 def formatModel():
     #take our json object and convert to tabular format
     model = {}  
-    with open('Models/distilled_water_model_absolute.json') as f:
+    with open('./Models/distilled_water_model_absolute.json') as f:
         model = f.read().replace('\n', '')
         model = json.loads(model)
     model_df = pd.DataFrame([], columns = ['Cond','PH', 'ORP', 'TDS', 'Turb', 'Desc', 'Timestamp', 'Contaminated'])  
@@ -39,3 +39,71 @@ def formatModel():
             
     
     return model_df
+
+def get_normalized_values():
+    cwd = os.getcwd()
+    print(cwd)
+    #take our json object and convert to tabular format
+    model = {}  
+    with open('../../Models/distilled_water_model_norm.json') as f:
+        model = f.read().replace('\n', '')
+        model = json.loads(model)
+        
+    model_df = pd.DataFrame([], columns = ['Cond','PH', 'ORP', 'TDS', 'Turb', 'Desc', 'Timestamp', 'Contaminated'])  
+    experiments =  model["Exps"]
+    ix = 0 #index of df
+    for exp in experiments:
+        insert = []
+        desc = exp["desc"]
+        ts = exp["timeStamp"]
+        contaminated = exp["contaminated"]
+        
+        for result in exp["results"]:
+            insert = []
+            insert.append(result["Conductivity"])
+            insert.append(result["PH"])
+            insert.append(result["ORP"])
+            insert.append(result["Turp"])
+            insert.append(result["TDS"])
+            insert.append(desc)
+            insert.append(ts)
+            insert.append(contaminated)
+            
+            model_df.loc[ix] = (insert)
+            ix += 1
+            
+    
+    return model_df
+
+def get_absolute_values():
+    #take our json object and convert to tabular format
+    model = {}  
+    with open('../../Models/distilled_water_model_absolute.json') as f:
+        model = f.read().replace('\n', '')
+        model = json.loads(model)
+    model_df = pd.DataFrame([], columns = ['Cond','PH', 'ORP', 'TDS', 'Turb', 'Desc', 'Timestamp', 'Contaminated'])  
+    experiments =  model["Exps"]
+    ix = 0 #index of df
+    for exp in experiments:
+        insert = []
+        desc = exp["desc"]
+        ts = exp["timeStamp"]
+        contaminated = exp["contaminated"]
+        
+        for result in exp["results"]:
+            insert = []
+            insert.append(result["Conductivity"])
+            insert.append(result["PH"])
+            insert.append(result["ORP"])
+            insert.append(result["Turp"])
+            insert.append(result["TDS"])
+            insert.append(desc)
+            insert.append(ts)
+            insert.append(contaminated)
+            
+            model_df.loc[ix] = (insert)
+            ix += 1
+            
+    
+    return model_df
+
