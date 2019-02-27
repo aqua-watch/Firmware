8a43b54f50829805af5aa8fdd4c2c02f2ff849fd
birdsoup <ccpowers@bu.edu>
2019-02-26 19:23:58 -0500

    refactor formatModels

--- web_interface/app.py
+++ web_interface/app.py
@@ -7,9 +7,10 @@ from flask import Markup
 import json
 import sys
 sys.path.insert(0, '../')
-from formatModels import load_model_json, get_values_from_json
+from formatModels import get_model_json, get_values_from_json
 #sys.path.append('arm/web_interface/')
 
+MODEL_DIR = "../Models/"
 NORM_MODEL_PATH = MODEL_DIR + "distilled_water_model_norm.json"
 ABS_MODEL_PATH =  MODEL_DIR + "distilled_water_model_absolute.json"
 
@@ -17,8 +18,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def display_content():
-    norm_json = load_model_json(NORM_MODEL_PATH)
-    abs_json = load_model_json(ABS_MODEL_PATH)
+    norm_json = get_model_json(NORM_MODEL_PATH)
+    abs_json = get_model_json(ABS_MODEL_PATH)
 
     norm_df = get_values_from_json(norm_json)
     absolute_df = get_values_from_json(abs_json)
@@ -77,31 +78,30 @@ def display_content():
             abs_tds.append(exp["center_point"]["TDS"])
             abs_turp.append(exp["center_point"]["Turp"])    
     
-    
-    
     norm_html = Markup(norm_df.to_html())
     absolute_html = Markup(absolute_df.to_html())
     
-    
-    
-    return render_template('content.html', normalized=norm_html, absolute=absolute_html, 
-                           times=json.dumps(times),
-                           norm_ph=norm_ph,
-                           norm_cond=norm_cond,
-                           norm_orp=norm_orp,
-                           norm_tds=norm_tds,
-                           norm_turp=norm_turp,
-                           norm_ph_std=norm_ph_std,
-                           norm_cond_std=norm_cond_std,
-                           norm_orp_std=norm_orp_std,
-                           norm_tds_std=norm_tds_std,
-                           norm_turp_std=norm_turp_std,
-                           abs_ph=abs_ph,
-                           abs_cond=abs_cond,
-                           abs_orp=abs_orp,
-                           abs_tds=abs_tds,
-                           abs_turp=abs_turp,
-                           ) 
+    return render_template(
+        'content.html', 
+        normalized=norm_html, 
+        absolute=absolute_html, 
+        times=json.dumps(times),
+        norm_ph=norm_ph,
+        norm_cond=norm_cond,
+        norm_orp=norm_orp,
+        norm_tds=norm_tds,
+        norm_turp=norm_turp,
+        norm_ph_std=norm_ph_std,
+        norm_cond_std=norm_cond_std,
+        norm_orp_std=norm_orp_std,
+        norm_tds_std=norm_tds_std,
+        norm_turp_std=norm_turp_std,
+        abs_ph=abs_ph,
+        abs_cond=abs_cond,
+        abs_orp=abs_orp,
+        abs_tds=abs_tds,
+        abs_turp=abs_turp,
+    ) 
     
 if __name__ == '__main__':
     #app.debug = True
